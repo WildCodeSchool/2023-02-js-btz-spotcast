2dd04bf7a06843043ea39c35df078bee21cedbf5
Olivier Gomez <ashitaaaka@gmail.com>
2023-04-13 18:31:08 +0200

    css initialisation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Bienvenue sur le projet Surf Météo (oui, le nom n'est pas terrible, je vous laisserai en choisir un nouveau :)
+      Bienvenue sur le projet Spotcast
     </div>
   );
 }
