cf33348c65a3f8f40dbe8fda1cb07ce95ed4af74
Olivier Gomez <ashitaaaka@gmail.com>
2023-04-14 19:22:08 +0200

    CSS and Cards initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Wind from './components/widgets/wind/Wind';
+
 
 function App() {
   return (
     <div className="App">
-      Bienvenue sur le projet Spotcast
+      <Wind />
     </div>
   );
 }
