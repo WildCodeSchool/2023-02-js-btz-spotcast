f32a6b62f62d9db99f9e35eeb4ce35a75825ca7e
Olivier Gomez <ashitaaaka@gmail.com>
2023-04-15 18:09:48 +0200

    Creating and styling Wind Widget

--- src/components/widgets/wind/Wind.jsx
+++ src/components/widgets/wind/Wind.jsx
@@ -10,7 +10,24 @@ const Wind = () => {
         <p className="card-title">Wind</p>
         <ToggleButton />
       </div>
-      <div className="card-content">Card-content</div>
+      <div className="card-content">
+        <div className="wind-chart">
+          <div className="cardinal-points">
+            <span id="north" style={{"--n":1}}></span>
+            <span id="north-east" className="checked" style={{"--n":2}}></span>
+            <span id="east" style={{"--n":3}}></span>
+            <span id="south-east" style={{"--n":4}}></span>
+            <span id="south" style={{"--n":5}}></span>
+            <span id="south-west" style={{"--n":6}}></span>
+            <span id="west" style={{"--n":7}}></span>
+            <span id="north-west" style={{"--n":8}}></span>
+          </div>
+          <div className="wind-stats">
+            <p className="wind-strenght">21</p>
+            <p>Km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
