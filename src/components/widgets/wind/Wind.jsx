7f5b5746417928ed735e392334dbd9fc320b9eeb
Olivier Gomez <ashitaaaka@gmail.com>
2023-04-16 00:15:04 +0200

    dynamic display of the wind speed function

--- src/components/widgets/wind/Wind.jsx
+++ src/components/widgets/wind/Wind.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Wind.css";
 import "../cards.css";
 import ToggleButton from "../../utilities/ToggleButton";
 
-const Wind = ({ time, winddirection_10m, windspeed_10m }) => {
-  console.log(time, winddirection_10m, windspeed_10m);
-  
+const Wind = ({winddirection_10m, windspeed_10m, timeStampIndex}) => {
+
+  const [windDirection, setWindDirection] = useState('')
+
+  // switch (winddirection_10m && winddirection_10m[timeStampIndex]){
+  //   case (winddirection_10m > 337 && winddirection_10m <= 22) :
+  //     setWindDirection('north');
+  //     break;
+  //   case (winddirection_10m > 22 && winddirection_10m <= 67) :
+  //     return setWindDirection('north-east');
+  //     break;
+  //   case (winddirection_10m > 67 && winddirection_10m <= 112) :
+  //     return setWindDirection('east');
+  //     break;
+  //   case (winddirection_10m > 112 && winddirection_10m <= 157) :
+  //     return setWindDirection('south-east');
+  //     break;
+  //   case (winddirection_10m > 157 && winddirection_10m <= 202) :
+  //     return setWindDirection('south-east');
+  //     break;
+  //   case (winddirection_10m > 202 && winddirection_10m <= 247) :
+  //     return setWindDirection('south');
+  //     break;
+  //   case (winddirection_10m > 247 && winddirection_10m <= 292) :
+  //     return setWindDirection('south-west');
+  //     break;
+  //   case (winddirection_10m > 292 && winddirection_10m <= 337) :
+  //     return setWindDirection('west');
+  //     break;
+  //   default:
+  //     setWindDirection('NO DATA');
+  //     break;
+  // }
+
+  console.log(winddirection_10m && windDirection)
+  // console.log(winddirection_10m && winddirection_10m[timeStampIndex])
+
   return (
     <div className="small-square">
       <div className="card-header">
@@ -25,7 +59,10 @@ const Wind = ({ time, winddirection_10m, windspeed_10m }) => {
             <span id="north-west" style={{"--n":8}}></span>
           </div>
           <div className="wind-stats">
-            <p className="wind-strenght">21</p>
+            <p className="wind-strenght">
+            {timeStampIndex&&
+            Math.floor(windspeed_10m[timeStampIndex])}
+            </p>
             <p>Km/h</p>
           </div>
         </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Wind from '../components/widgets/wind/Wind'
-import './Dashboard.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Wind from "../components/widgets/wind/Wind";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    const[date, setDate] = useState(new Date());
-    //Setting up a realtime clock
-    useEffect(()=>{
-        const timer = setInterval(() => setDate(new Date()), 1000 )
-        return function cleanup(){
-            clearInterval(timer)
-        }
-    })
+  //Setting up a realtime clock
+  const [date, setDate] = useState(new Date());
+  
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
 
-    const [wind, setWind] = useState([])
-    //fetching the wind infos
-    useEffect(()=>{
-        axios
-        .get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_10m,winddirection_10m')
-        .then((res) => res.data)
-        .then((data)=>{
-            setWind(data.hourly)
-        })
-    },[])
-    
+  //fetching the wind infos
+  const [wind, setWind] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_10m,winddirection_10m"
+      )
+      .then((res) => res.data)
+      .then((data) => {
+        setWind(data.hourly);
+      });
+  }, []);
+
+  //getting the index of current time in 'wind' array
+  const [timeStampIndex, setTimeStampIndex] = useState('');
+
+  const timeStamp = 
+  `${date.getFullYear()}-${String(date.getMonth() +1).padStart(2,"0")}-${String(date.getDate()).padStart(2,"0")}T${String(date.getHours()).padStart(2,"0")}:00`;
+
+
+  useEffect(() => {
+    wind.time && //checking if 'wind.time' is already loaded
+    setTimeStampIndex(wind.time.indexOf(timeStamp));
+  }, [wind.time]); //setup timeStampIndex after ' wind.time' is updated
+
   return (
-    <div className='dashboard'>
-        <Wind {...wind}/>
-        <p>{date.toLocaleTimeString()}</p>
+    <div className="dashboard">
+      <Wind 
+        {...wind} 
+        timeStampIndex={timeStampIndex}
+        />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
