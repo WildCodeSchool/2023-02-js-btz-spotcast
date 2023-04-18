480b4ffc83170f30a7bbe18374e8e1bcef94417a
Eric Godefroy <ericgodefroy@outlook.fr>
2023-04-18 09:12:33 +0200

    Maj de l'architecture du site + création useState pour les API

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,10 +3,21 @@ import axios from "axios";
 import Wind from "../components/widgets/wind/Wind";
 import "./Dashboard.css";
 
+
+
 const Dashboard = () => {
+
+  //setting up Selected Spot 
+  const [selectedSpots, setSelectedSpots] = useState([{
+    name : "Biarritz - La Côte des Basques",
+    latitude : "43.48",
+    longitude : "-1.56",
+  }])
+
+
   //Setting up a realtime clock
   const [date, setDate] = useState(new Date());
-  
+
   useEffect(() => {
     const timer = setInterval(() => setDate(new Date()), 60000);
     return function cleanup() {
@@ -20,20 +31,21 @@ const Dashboard = () => {
   useEffect(() => {
     axios
       .get(
-        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_10m,winddirection_10m"
+        `https://api.open-meteo.com/v1/forecast?latitude=${selectedSpots[0].latitude}&longitude=${selectedSpots[0].longitude}&hourly=windspeed_10m,winddirection_10m`
       )
       .then((res) => res.data)
       .then((data) => {
         setWind(data.hourly);
-      });
+      }); 
   }, []);
+
+  
 
   //getting the index of current time in 'wind' array
   const [timeStampIndex, setTimeStampIndex] = useState('');
 
   const timeStamp = 
   `${date.getFullYear()}-${String(date.getMonth() +1).padStart(2,"0")}-${String(date.getDate()).padStart(2,"0")}T${String(date.getHours()).padStart(2,"0")}:00`;
-
 
   useEffect(() => {
     wind.time && //checking if 'wind.time' is already loaded
