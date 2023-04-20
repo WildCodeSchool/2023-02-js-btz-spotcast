0137867efebca3d187f3a745cbc6afcba9f35263
Olivier Gomez <ashitaaaka@gmail.com>
2023-04-20 19:00:05 +0200

    Sunset widget done and stylized

--- src/components/widgets/sunset/Sunset.jsx
+++ src/components/widgets/sunset/Sunset.jsx
@@ -1,22 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ToggleButton from '../../utilities/ToggleButton'
 import './Sunset.css'
+import axios from 'axios'
+import Aube from '../../../assets/images/aube.svg'
+import Crepuscule from '../../../assets/images/crepuscule.svg'
+import Separator from '../../../assets/images/sunset-separator.svg'
 
 const Sunset = () => {
+    
+    const [sunset, setSunset] = useState();
+
+    useEffect(() =>{
+        axios
+        .get("https://api.sunrise-sunset.org/json?lat=43.4831519&lng=-1.558626&formatted=0")
+        .then((res) => res.data.results)
+        .then((data) =>{
+            setSunset(data)
+        })
+    },[])
+    
+    let sunriseHours = String(new Date(sunset ? sunset.civil_twilight_begin : "").getHours());
+    let sunriseMinutes = String(new Date(sunset ? sunset.civil_twilight_begin : "").getMinutes());
+    let sunriseFormatedTime = `${sunriseHours.padStart(2,'0')}:${sunriseMinutes.padStart(2,'0')}`;
+
+    let sunsetHours = String(new Date(sunset ? sunset.civil_twilight_end : "").getHours());
+    let sunsetMinutes = String(new Date(sunset ? sunset.civil_twilight_end : "").getMinutes());
+    let sunsetFormatedTime = `${sunsetHours.padStart(2,'0')}:${sunsetMinutes.padStart(2,'0')}`;
+
+    
+
+
   return (
     <div className='big-rectangle-width-sunset'>
-        <div className="sunset-left">
+        <div className="sunrise">
             <p className="card-title-sunset">Sun cycle</p>
-            <div className="sunset-forecast">
-                <img src="" alt="" className="sun" />
-                <p className="sunset">Sunrise</p>
+            <div className="sunrise-forecast">
+                <img src={Aube} alt="sun" className="sun" />
+                <div className='sunset-forecast-container'>
+                    <p className="sunrise-text">
+                        Dawn
+                    </p>
+                    <p className="sunrise-value">
+                        {sunset ? sunriseFormatedTime : ""}
+                    </p>
+                </div>
             </div>
         </div>
-        <div className="sunset-right">
+        <div className="separator">
+            <img src={Separator} alt="separator" />
+        </div>
+        <div className="sunset">
             <ToggleButton className="sunset-toggle" />
-            <div className="sunrise-forecast">
-                <img src="" alt="" className="sun" />
-                <p className="sunrise">Sunrise</p>
+            <div className="sunset-forecast">
+                <img src={Crepuscule} alt="moon" className="moon" />
+                <div className='sunset-forecast-container'>
+                    <p className="sunset-text">
+                       Dusk
+                    </p>
+                    <p className="sunset-value">
+                        {sunset ? sunsetFormatedTime : ""}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
